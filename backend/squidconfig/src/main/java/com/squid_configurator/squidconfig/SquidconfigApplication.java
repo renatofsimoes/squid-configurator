0429f2aca2eb763a7a2375552ae3772a61cfb6f9
Renato Fioroni Simoes <renatofsim@gmail.com>
2025-07-14 11:55:33 -0300

    Criada estrutura inicial

---
+++ backend/squidconfig/src/main/java/com/squid_configurator/squidconfig/SquidconfigApplication.java
@@ -0,0 +1,13 @@
+package com.squid_configurator.squidconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquidconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquidconfigApplication.class, args);
+	}
+
+}
---
+++ backend/squidconfig/src/test/java/com/squid_configurator/squidconfig/SquidconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squid_configurator.squidconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquidconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
