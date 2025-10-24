918adeec26cd059ad8ddd1f1bed3b28493167b3d
Renato Fioroni Simoes <renatofsim@gmail.com>
2025-10-24 10:47:25 -0300

    Ajustes de css no form das diretivas

--- frontend/squidconfig-ui/src/components/AclDirectivesForm.jsx
+++ frontend/squidconfig-ui/src/components/AclDirectivesForm.jsx
@@ -8,9 +8,24 @@ const AclDirectivesForm = ({ onBack }) => {
       <form id="directives-form">
         <h2>Adicionar Diretiva</h2>
         <div id="directives-top">
-          <label></label>
-          <input placeholder="Nome da diretiva" />
-          <input placeholder="Valor da diretiva" />
+          <div className="directives-select">
+            <label>Tipo de Diretiva:</label>
+            <select>
+              <option value="http_access">Http_access</option>
+              <option value="http_reply_access">Http_reply_access</option>
+              <option value="url_rewrite_access">Url_rewrite_access</option>
+              <option value="delay_access">Delay_access</option>
+              <option value="cache">Cache</option>
+              <option value="access_log">Access_log</option>
+            </select>
+          </div>
+          <div className="directives-select">
+            <label>Ação:</label>
+            <select>
+              <option value="allow">Allow</option>
+              <option value="deny">Deny</option>
+            </select>
+          </div>
         </div>
         <div id="directives-btns">
           <Button
